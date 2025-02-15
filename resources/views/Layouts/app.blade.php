55052a24f98702ffae6b08316d68124bc11d2279
Yirbel Gomez <yirbelg@gmail.com>
2025-02-15 14:58:52 -0400

    cambios leves en el front

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -3,11 +3,18 @@
 <head>
     <title>@yield('title')</title>
     @vite('resources/sass/app.scss')
+    <style>
+        html {
+            background-color: #F6F5FA;
+        }
+    </style>
 </head>
 <body>
-    <div id="app" class="container">
+    <div id="app" class="">
         @yield('content')
     </div>
     @vite('resources/js/app.js')
 </body>
 </html>
+
+
