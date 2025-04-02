8fec2ad01028b61d7d575382750fcbe554761b1e
Yirbel Gomez <yirbelg@gmail.com>
2025-04-01 22:28:15 -0400

    Estilo de la imagen de noticia mejorado, se le puso un no subtitle available cuando la pg web no tenga subtitulos en su noticia, swimmingNews Primer Scrapping hecho

--- resources/js/components/Noticias/NoticiasView.vue
+++ resources/js/components/Noticias/NoticiasView.vue
@@ -225,9 +225,23 @@ export default {
 
     async getVoleyballNews() {
       try {
-        const response = await axios.get('/voleyball_news');
+        const response = await axios.get('/volleyball_news');
 
         this.noticias = response.data.volleyball_news;
+      }
+      catch (error) {
+        console.error('Error al obtener las noticias:', error);
+        this.errorMessage = 'Algo salió mal al cargar las noticias. Por favor, intenta de nuevo más tarde.';
+      } finally {
+        this.isLoading = false;
+      }
+    },
+
+    async getSwimmingNews() {
+      try {
+        const response = await axios.get('/swimming_news');
+
+        this.noticias = response.data.swimming_news;
       }
       catch (error) {
         console.error('Error al obtener las noticias:', error);
@@ -244,6 +258,7 @@ export default {
     this.getFutbolNews(); 
     this.getBasketballNews();
     this.getVoleyballNews();
+    this.getSwimmingNews()
   },
 };
 </script>
