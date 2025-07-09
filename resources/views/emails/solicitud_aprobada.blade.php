3aa02b8dd0dee396ca2378a07c531d363c65efcd
Yirbel Gomez <yirbelg@gmail.com>
2025-07-08 22:15:42 -0400

    message

--- resources/views/emails/solicitud_aprobada.blade.php
+++ resources/views/emails/solicitud_aprobada.blade.php
@@ -1,4 +1,4 @@
 <p>Hola {{ $user->name }},</p>
 <p>¡Tu solicitud de entrenamiento ha sido <b>aprobada</b>!</p>
-<p>Puedes comunicarte con tu entrenador para coordinar los detalles.</p>
+<p> Ya Puedes comunicarte con tu entrenador via chat para coordinar los detalles.</p>
 <p>¡Éxito!</p>
