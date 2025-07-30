0668224d792c64cb8f1d48ede1b03b94215d6d03
Yirbel Gomez <yirbelg@gmail.com>
2025-07-29 22:56:39 -0400

    emails

---
+++ app/Mail/SolicitudRechazadaEntrenador.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SolicitudRechazadaaEntrenador extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $entrenador;
+
+    public function __construct($entrenador)
+    {
+        $this->entrenador = $entrenador;
+    }
+
+    public function build()
+    {
+        return $this->subject('¡Tu solicitud de ser entrenador fue rechazada!')
+            ->view('emails.solicitud_rechazada_entrenador');
+    }
+}
